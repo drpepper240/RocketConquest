57e1423ae35fd1617a2eafc50dff1a5227ac96cd
drpepper240 <drpepper240@PC>
2017-10-16 00:54:44 +0300

    Initial

---
+++ ConquestTest/CommandGetCoordinates.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+
+namespace Conquest
+{
+	public class CommandGetCoordinates : IRocketCommand
+	{
+		public string Name
+		{
+			get { return "gc"; }
+		}
+
+		public string Help
+		{
+			get { return ("Get coordinates"); }
+		}
+
+		public AllowedCaller AllowedCaller
+		{
+			get { return AllowedCaller.Player; }
+		}
+
+		public string Syntax
+		{
+			get { return "Usage: /gc"; }
+		}
+
+		public List<string> Aliases
+		{
+			get { return new List<string>() { }; }
+		}
+
+		public List<string> Permissions
+		{
+			get { return new List<string>() { "Conquest.permission" }; }
+
+		}
+
+		public void Execute(IRocketPlayer caller, string[] command)
+		{
+			UnturnedPlayer up = (UnturnedPlayer)caller;
+			if (Conquest.instance == null || up == null)
+			{
+				UnturnedChat.Say("Something's wrong with the plugin!", UnityEngine.Color.red);
+				return;
+			}
+
+			UnturnedChat.Say(caller, "X = " + up.Position.x.ToString());
+			UnturnedChat.Say(caller, "Y = " + up.Position.y.ToString());
+			UnturnedChat.Say(caller, "Z = " + up.Position.z.ToString());
+		}
+	}
+}
---
+++ ConquestTest/PlayerComponentConquest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.Unturned.Player;
+using UnityEngine;
+
+namespace Conquest
+{
+	public class PlayerComponentConquest : UnturnedPlayerComponent
+	{
+		UInt32 lastUpdatedTicks; //TODO initial value
+
+
+		private bool isInside(Vector3 pos, Vector3 min, Vector3 max)
+		{
+			return (pos.x > min.x && pos.x < max.x && pos.y > min.y && pos.y < max.y); //TODO z axis setting
+		}
+		private void FixedUpdate()
+		{
+			if (lastUpdatedTicks < Conquest.instance.Configuration.Instance.ticksUpdatePlayerPosition)
+			{
+				lastUpdatedTicks++;
+				return;
+			}
+			else
+			{
+				lastUpdatedTicks = 0;
+				//TODO update player position
+				if (isInside(this.Player.Position, Conquest.instance.Configuration.Instance.zoneMin, Conquest.instance.Configuration.Instance.zoneMax))
+				{
+					Conquest.instance.zoneList.Add(this.Player.CSteamID, this.Player.SteamGroupID.m_SteamID);
+				}
+				else
+				{
+					Conquest.instance.zoneList.Remove(this.Player.CSteamID);
+				}
+			}
+		}
+	}
+}
+
